7bb9c3ee001d0064046085366f315693f1a44ad8
Kian Gorgichuk <kgorgi@uvic.ca>
2018-08-31 11:29:40 -0600

    Add GraphLegend implementation

--- src/components/shell/graph/GraphLegend.tsx
+++ src/components/shell/graph/GraphLegend.tsx
@@ -1,11 +1,52 @@
 import * as React from 'react';
 
+import Arrow from './Arrow';
+
 import "./css/GraphLegend.css";
 
 class GraphLegend extends React.Component {
     public render() {
         return (
-            <div className="GraphLegend"/>
+            <div className="GraphLegend">
+                <div className="GraphLegend-text">
+                    Prerequisite:
+                    <Arrow
+                        angle={45}
+                        color="blue"
+                        length={40}
+                        lineWidth={2}
+                        style={{
+                            width: '70px'
+                          }}
+                    />
+                </div>
+
+                <div className="GraphLegend-text">
+                    Corequisite: 
+                    <Arrow
+                        angle={45}
+                        color="red"
+                        length={40}
+                        lineWidth={2}
+                        style={{
+                            width: '70px'
+                          }}
+                    />
+                </div>
+
+                <div className="GraphLegend-text">
+                    Pre/co-requisite: 
+                    <Arrow
+                        angle={45}
+                        color="green"
+                        length={40}
+                        lineWidth={2}
+                        style={{
+                            width: '70px'
+                          }}
+                    />
+                </div>
+            </div>
         );
     }
 }
