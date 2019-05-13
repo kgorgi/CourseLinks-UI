d76b79c7878757ca270b03f7cbd0e1472b181d1e
Kian Gorgichuk <kgorgi@uvic.ca>
2019-05-12 22:49:22 -0700

    Refactor overlay with help text

---
+++ src/components/shell/MinSizeOverlay.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+import './css/MinSizeOverlay.css'
+
+export function MinSizeOverlay() {
+    return (
+        <div className='MinSizeOverlay'>
+            <h1>
+                Welcome to Course Links!
+        </h1>
+            <h3>
+                Your window size is too small. <br /> 
+                Please increase the window size or zoom out. <br /> <br />
+                CourseLinks requires a widescreen display and <br />
+                does not support mobile devices at this time.
+        </h3>
+        </div>
+    )
+}
